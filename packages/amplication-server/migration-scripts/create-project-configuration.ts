080a3649387579cf9481fd74b77caf3ad22f7cc9
tupe12334 <ofek.gabay.he@gmail.com>
2022-09-04 13:42:31 +0300

    fix

--- packages/amplication-server/migration-scripts/create-project-configuration.ts
+++ packages/amplication-server/migration-scripts/create-project-configuration.ts
@@ -6,58 +6,9 @@ import {
 
 import { DEFAULT_RESOURCE_COLORS } from '../src/core/resource/constants';
 
-  const projects = await client.project.findMany({
-    where: {
-      resources: {
-        none: {
-          resourceType: EnumResourceType.ProjectConfiguration
-        }
-      }
-    },
-    include: {
-      resources: true
-    }
-  });
-
-  console.log(projects.length);
-  let index = 1;
-
-  const chunks = chunkArrayInGroups(projects, 500);
-
-  for (let chunk of chunks) {
-    console.log(index++);
-    await migrateChunk(chunk);
-  }
-
-  async function migrateChunk(chunk) {
-    const promises = chunk.map(async project => {
-      return client.blockVersion.create({
-        data: {
-          versionNumber: 0,
-          displayName: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME,
-          description: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION,
-          inputParameters: {},
-          outputParameters: {},
-          settings: blockVersionSettings,
-          block: {
-            create: {
-              blockType: EnumBlockType.ProjectConfigurationSettings,
-              description: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_DESCRIPTION,
-              displayName: DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME,
-              resource: {
-                create: {
-                  color: DEFAULT_RESOURCE_COLORS.projectConfiguration,
-                  resourceType: EnumResourceType.ProjectConfiguration,
-                  description: DEFAULT_PROJECT_CONFIGURATION_DESCRIPTION,
-                  name: DEFAULT_PROJECT_CONFIGURATION_NAME,
-                  project: { connect: { id: project.id } }
-                }
-              }
-            }
-          }
-        }
-      });
-    });
+const DEFAULT_PROJECT_CONFIGURATION_NAME = 'Project Configuration';
+const DEFAULT_PROJECT_CONFIGURATION_DESCRIPTION =
+  'This resource is used to store project configuration.';
 
 const DEFAULT_PROJECT_CONFIGURATION_SETTINGS_NAME =
   'Project Configuration Settings';
